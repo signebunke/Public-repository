3ee504dfc12ce2dd07147d3543732cbda227e5f1
Signe Bunke <signebunke@gmail.com>
2021-01-21 21:04:39 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,18 @@
 class QuestionsController < ApplicationController
 
   def ask
+
   end
 
   def answer
-  end
+    user_question = params[:question]
 
+    if user_question == 'I am going to work'
+      @response = 'Great!'
+    elsif user_question[-1] == '?'
+      @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
 end
